75491ee53b80c7b9e0a5246c4826a036ddf8172a
Nabeel Wasif <hassanfarhat986@gmail.com>
2024-12-16 23:02:49 +0530

    Removed tokenGenerator

--- lib/bucket.ts
+++ lib/bucket.ts
@@ -1,7 +1,9 @@
-import { Storage } from '@google-cloud/storage';
+import { Storage, StorageOptions } from '@google-cloud/storage';
+import { getServiceAccount } from '@/utils/generateCredential';
 
-
-const storage = new Storage();
+const token = getServiceAccount();
+const options: StorageOptions = { token: token }
+const storage = new Storage(options);
 const bucketName = 'tap-up-bucket';
 const bucket = storage.bucket(bucketName);
 
