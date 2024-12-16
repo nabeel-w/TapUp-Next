285460f45eb62c3cee9f60921684a158d6d3e6dd
Nabeel Wasif <hassanfarhat986@gmail.com>
2024-12-17 00:22:56 +0530

    Fixed config Leak

--- lib/bucket.ts
+++ lib/bucket.ts
@@ -1,6 +1,8 @@
+import { getGCPCredentials } from '@/utils/configHelper';
 import { Storage } from '@google-cloud/storage';
 
-const storage = new Storage();
+
+const storage = new Storage(getGCPCredentials());
 const bucketName = 'tap-up-bucket';
 const bucket = storage.bucket(bucketName);
 
